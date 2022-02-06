f2df5aebe8c08e66d0d653fadd6975c41e3527bd
Anna Hoffa <60316298+annahoffa@users.noreply.github.com>
2022-02-06 15:12:41 +0100

    Create & style the sign-in page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component';
 import Header from './components/header/header.component';
 
 import './App.css';
@@ -15,6 +16,7 @@ class App extends React.Component {
 				<Switch>
 					<Route exact path='/' component={HomePage} />
 					<Route path='/shop' component={ShopPage} />
+					<Route path='/signin' component={SignInAndSignUpPage} />
 				</Switch>
 			</div>
 		);
