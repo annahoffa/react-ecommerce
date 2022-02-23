312323aa6306a819c69a58d1ec516b33a76cdd76
annahoffa <60316298+annahoffa@users.noreply.github.com>
2022-02-23 15:44:48 +0100

    Add Redux and implement the userReducer & userActions

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { auth } from '../../firebase/firebase.utils'
+import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -21,6 +22,10 @@ const Header = ({ currentUser }) => (
       }
     </div>
   </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
